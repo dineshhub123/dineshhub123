ca8f1cae8637af44e33ff47ebb772625bb80b67a
DineshBhagat <dineshbhagatbpl@gmail.com>
2020-04-18 12:50:16 +1000

    all changes file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import 'rxjs/add/operator/catch';
 @Component({
   selector: 'app-root',
   templateUrl:'./app.component.html',  
-  
+  styleUrls:['./app.component.css'],
 })
 export class AppComponent {
   public result:any;
